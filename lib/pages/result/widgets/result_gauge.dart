ebea3360048af6d442639bb3462485aa9a7093e8
111coding <jwlee1024@gmail.com>
2024-09-30 07:23:23 +0900

    add ResultGauge

--- lib/pages/result/widgets/result_gauge.dart
+++ lib/pages/result/widgets/result_gauge.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ResultGauge extends StatelessWidget {
@@ -5,6 +7,32 @@ class ResultGauge extends StatelessWidget {
   final double result;
   @override
   Widget build(BuildContext context) {
-    return Text('ResultGauge');
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        // 가로 세로가 같은 SizedBox를 사용할 때
+        SizedBox.square(
+          dimension: 250,
+          child: CircularProgressIndicator(
+            value: 1,
+            strokeWidth: 2,
+            color: Theme.of(context).dividerColor,
+          ),
+        ),
+        SizedBox.square(
+          dimension: 250,
+          child: CircularProgressIndicator(
+            value: min((result / 35), 1), // BMI max 35!
+            strokeWidth: 3,
+            color: Theme.of(context).highlightColor,
+          ),
+        ),
+        // Stack 의 크기가 자녀위젯의 크기에 따라 달라지므로 Center 사용 불가
+        Text(
+          result.toStringAsFixed(1),
+          style: TextStyle(fontSize: 20),
+        ),
+      ],
+    );
   }
 }
