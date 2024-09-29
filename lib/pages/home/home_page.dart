acd28ae93490e9cebf187bad2a081ef4d5c1d65d
111coding <jwlee1024@gmail.com>
2024-09-30 06:24:10 +0900

    WIP state GenderBox

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,7 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bmi_app/pages/home/widgets/gender_box.dart';
 import 'package:flutter_bmi_app/pages/home/widgets/slider_box.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isMale = true;
+  void onGenderChanged(bool male) {
+    setState(() {
+      isMale = male;
+    });
+  }
+
+  double height = 173;
+  double weight = 72;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +30,7 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 50),
-            GenderBox(),
+            GenderBox(isMale, onGenderChanged),
             Spacer(),
             SliderBox(label: 'HEIGHT', value: 173, unit: 'cm'),
             Spacer(),
--- lib/pages/home/widgets/gender_box.dart
+++ lib/pages/home/widgets/gender_box.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class GenderBox extends StatelessWidget {
+  const GenderBox(this.isMale, this.onChanged);
+  final bool isMale;
+  final void Function(bool) onChanged;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        gender(context, Icons.male, 'MALE', true),
+        gender(context, Icons.male, 'MALE', isMale, () {
+          onChanged(true);
+        }),
         SizedBox(width: 8),
-        gender(context, Icons.female, 'FEMALE', false),
+        gender(context, Icons.female, 'FEMALE', !isMale, () {
+          onChanged(false);
+        }),
       ],
     );
   }
@@ -17,38 +24,42 @@ class GenderBox extends StatelessWidget {
     IconData icon,
     String text,
     bool selected,
+    void Function() onTap,
   ) {
     return Expanded(
-      child: Container(
-        height: 150,
-        decoration: BoxDecoration(
-          border: Border.all(color: Theme.of(context).dividerColor),
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 10,
-              left: 10,
-              child: Opacity(
-                opacity: 0.3,
-                child: Icon(icon, size: 80),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(color: Theme.of(context).dividerColor),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 10,
+                left: 10,
+                child: Opacity(
+                  opacity: 0.3,
+                  child: Icon(icon, size: 80),
+                ),
               ),
-            ),
-            Positioned(
-              right: 20,
-              bottom: 20,
-              child: Opacity(
-                opacity: selected ? 1 : 0.3,
-                child: Text(
-                  text,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+              Positioned(
+                right: 20,
+                bottom: 20,
+                child: Opacity(
+                  opacity: selected ? 1 : 0.3,
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
